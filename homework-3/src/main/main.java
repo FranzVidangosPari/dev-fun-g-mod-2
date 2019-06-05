90374d465ed2dd8fa554cef73d8748e151c64d91
FranzVidangosPari <Franz.Vidangos@jalasoft.com>
2019-06-05 17:40:46 -0400

    tree generic
    add and print

---
+++ homework-3/src/main/main.java
@@ -0,0 +1,16 @@
+public class main {
+    public static void main(String[] arg){
+        CustomTree<String> newCustomTree = new CustomTree<>();
+        newCustomTree.add("50");
+        newCustomTree.add("25");
+        newCustomTree.add("75");
+
+        newCustomTree.add("15");
+        newCustomTree.add("35");
+        newCustomTree.add("55");
+        newCustomTree.add("85");
+
+        newCustomTree.print();
+
+    }
+}
